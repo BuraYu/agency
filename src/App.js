3ff6a2cf4e31286d8023c61ff23f5406520c7b51
BuraYu <burak.yueksel@hotmail.com>
2024-06-11 23:11:29 +0200

    add navbar and background to hero section

--- src/App.js
+++ src/App.js
@@ -3,7 +3,32 @@ import "./sass/style.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div className="container">
+        {/* Hero section */}
+        <section className="hero">
+          <div className="header-bar">
+            <span className="logo">sunnyside</span>
+            <nav className="navigation">
+              <ul>
+                <li>
+                  <a href="#">About</a>
+                </li>
+                <li>
+                  <a href="#">Services</a>
+                </li>
+                <li>
+                  <a href="#">Projects</a>
+                </li>
+                <li className="contact-list">
+                  <a href="#" className="contact">
+                    Contact
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
